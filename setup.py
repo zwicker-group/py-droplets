ade8c4f4bbdee8b52d0f7d6bce546f900b0939c1
David Zwicker <david.zwicker@ds.mpg.de>
2020-03-03 09:59:58 +0100

    Prepare pypi publication

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'py-droplets',
+  package_data={"droplets": ["py.typed"]},
+  packages = find_packages(),
+  zip_safe=False,  # this is required for mypy to find the py.typed file
+  version = '0.1',
+  license='MIT',
+  description = 'Python package for describing and analyzing droplets in experiments and simulations',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'David Zwicker',
+  author_email = 'david.zwicker@ds.mpg.de',
+  url = 'https://github.com/zwicker-group/py-droplets',
+  download_url = 'https://github.com/zwicker-group/py-droplets/archive/v0.1.tar.gz',
+  keywords = ['emulsions', 'image-analysis'],
+  python_requires='>=3.6',
+  install_requires=['matplotlib',
+                    'numpy',
+                    'numba',
+                    'scipy',
+                    'sympy',
+                    'py-pde'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
