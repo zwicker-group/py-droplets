a3b0014df49a593becfa1f1bdd78cefbae455493
David Zwicker <david.zwicker@ds.mpg.de>
2020-05-04 09:59:34 +0200

    Add examples to test

--- examples/analyze_image.py
+++ examples/analyze_image.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+
 from pde.fields import ScalarField
 from droplets.image_analysis import locate_droplets
 
-field = ScalarField.from_image('resources/emulsion.png')
+img_path = Path(__file__).parent / 'resources' / 'emulsion.png'
+field = ScalarField.from_image(img_path)
 emulsion = locate_droplets(field)
 
 for droplet in emulsion:
