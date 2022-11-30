8235034c6f3a4a44084473c67dcf4a86b4be737d
David Zwicker <david.zwicker@ds.mpg.de>
2022-11-30 16:12:50 +0100

    Additional fixes

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-#
 # This script formats the code of this package
-#
 
-# format imports
 echo "Formating import statements..."
 isort ..
 
-# format rest of the code
-for dir in droplets examples ; do
-    echo "Formating files in ${dir}..."
-    black ../${dir}
-done
+echo "Formating source code..."
+black ..
