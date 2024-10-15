4d94664ae6998923e6432daa85cb101a2217dff4
David Zwicker <david.zwicker@ds.mpg.de>
2024-10-15 15:05:26 +0200

    Added decorator to support scalar arguments in methods of droplet class (#71)
    This functionality was ported from `py-pde` where it is now deprecated.
    
    * Also bumped version of github actions

---
+++ tests/tools/test_misc.py
@@ -0,0 +1,24 @@
+"""
+.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
+"""
+
+import numpy as np
+from scipy import integrate
+
+from droplets.tools import misc
+
+
+def test_enable_scalar_args():
+    """Test the enable_scalar_args decorator."""
+
+    class Test:
+        @misc.enable_scalar_args
+        def meth(self, arr):
+            return arr + 1
+
+    t = Test()
+
+    assert t.meth(1) == 2
+    assert isinstance(t.meth(1), float)
+    assert isinstance(t.meth(1.0), float)
+    np.testing.assert_equal(t.meth(np.ones(2)), np.full(2, 2))
